848c9af230ff9e699e9611b524359442e2bd55b7
NekomimiGaDaisuki <minhnd2002@yahoo.com.vn>
2023-10-13 22:08:40 +0700

    LTUD Xây Dựng
    Đồ án nhóm 6

--- LTUDXayDung/Form1.cs
+++ LTUDXayDung/Form1.cs
@@ -15,5 +15,10 @@ namespace LTUDXayDung
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
